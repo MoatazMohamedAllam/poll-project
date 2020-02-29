7520a8f8ebd9e48cb94b623bf18df66a82f6bdaa
MoatazMohamedAllam <mezo.allam2015@hotmail.com>
2020-02-29 14:33:18 +0200

    my first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moataz14/Desktop/myprojects/django-projects/Poll-project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
